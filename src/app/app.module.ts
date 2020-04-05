93528868f748dea67e075b91d484043a867b8ea3
trytosend <trytosend@centrum.sk>
2020-04-05 23:05:28 +0200

    feat(form): display questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { StepperComponent } from './stepper/stepper.component';
 import { FormComponent } from './form/form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { FormComponent } from './form/form.component';
     BrowserAnimationsModule,
     MatStepperModule,
     MatIconModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { SectionI } from 'src/models/section';
+import { SectionI, QuestionI } from 'src/models/section';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,9 +11,19 @@ export class FormComponent implements OnInit {
   @Input() section: SectionI;
   @Input() formName: string;
 
-  constructor() { }
+  public sectionForm;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+  }
 
   ngOnInit(): void {
+    const group = {};
+    this.section.questions.forEach((question: QuestionI) => {
+      group[question.text] = new FormControl('');
+    });
+    this.sectionForm = this.formBuilder.group(group);
   }
 
   showMeValue(event: any): void {
